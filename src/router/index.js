cb6540453b94f379c5b757dbc8f737d232b6cc09
SunsetMT <grisha.serebrov@mail.ru>
2021-12-18 14:50:23 +0300

    some fixes + update

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import PageWrapper from "@/components/PageWrapper";
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: PageWrapper
   },
   {
     path: '/about',
